efb15cf07ce903c218e727856b6b4439ea72252e
ririsriskyaulia <ririsriskyaulia@gmail.com>
2022-04-20 06:40:26 +0700

    Shared Preference

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:shopcart/pages/login.dart';
+import 'models/keranjang.dart';
+
+void main() async {
+  Hive.initFlutter();
+  Hive.registerAdapter(ItemAdapter());
+  await Hive.openBox<Item>('items');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  static final String _title = 'Shared Preferences & Hive';
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: _title,
+
+    theme: ThemeData(
+      primarySwatch: Colors.grey,
+    ),
+    home: const LoginPage(),
+  );
+}
